217948903354a0de42c6854fec9d9f013bd65a18
Bayardo Lopez <bjlslopez@gmail.com>
2025-04-26 16:24:07 -0600

    BlogPost serializer + viewset

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('blog-posts/', views.BlogPostListAPIView.as_view()),
+    # path('blog-posts/', views.BlogPostListAPIView.as_view()),
 ]
+
+router = DefaultRouter()
+router.register('blog-posts', views.BlogPostViewSet)
+urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from api.serializers import BlogPostSerializer
 from api.models import BlogPost
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import (
+    AllowAny
+)
 
-class BlogPostListAPIView(generics.ListAPIView):
+
+class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    permission_classes = [AllowAny]
