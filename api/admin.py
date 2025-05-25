995608d2de116c37580484b0cc143d5111306404
Bayardo Lopez <bjlslopez@gmail.com>
2025-05-25 12:04:50 -0600

    Added contact form submission endpoint

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from api.models import BlogPost
+from api.models import BlogPost, Contact
 
 
 class BlogPostAdmin(MarkdownxModelAdmin):
@@ -10,6 +10,17 @@ class BlogPostAdmin(MarkdownxModelAdmin):
         css = {
             'all': ('css/admin_custom.css',)
         }
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at')
+    readonly_fields = ('name', 'email', 'phone', 'message', 'created_at')
+    ordering = ('-created_at',)
+    search_fields = ('name', 'email', 'message')
+
+    def has_add_permission(self, request):
+        return False
 
 
 admin.site.register(BlogPost, BlogPostAdmin)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,19 @@
 from api.serializers import BlogPostSerializer
+from api.serializers import ContactSerializer
 from api.models import BlogPost
-from rest_framework import viewsets
+from api.models import Contact
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 
 
 class BlogPostViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BlogPost.objects.filter(published=True).order_by('-created_at')
     serializer_class = BlogPostSerializer
     lookup_field = 'slug'
+
+
+class ContactViewSet(mixins.CreateModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [AllowAny]
